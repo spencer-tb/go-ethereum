5887918736a86ad93d2a2bce0b488df532b772d5
Marius van der Wijden <m.vanderwijden@live.de>
2023-06-09 14:58:51 +0200

    core/types: marshall receipts correctly

--- core/types/receipt.go
+++ core/types/receipt.go
@@ -305,6 +305,9 @@ func (rs Receipts) EncodeIndex(i int, w *bytes.Buffer) {
 	case DynamicFeeTxType:
 		w.WriteByte(DynamicFeeTxType)
 		rlp.Encode(w, data)
+	case BlobTxType:
+		w.WriteByte(BlobTxType)
+		rlp.Encode(w, data)
 	default:
 		// For unsupported types, write nothing. Since this is for
 		// DeriveSha, the error will be caught matching the derived hash
