6a368067c343c34e86d9f28f5b3b30a7082500f8
spencer-tb <spencer@spencertaylorbrown.uk>
2024-11-26 17:12:44 +0700

    chore: add 4884 bug.

--- params/protocol_params.go
+++ params/protocol_params.go
@@ -173,7 +173,7 @@ const (
 	BlobTxPointEvaluationPrecompileGas = 50000   // Gas price for the point evaluation precompile.
 
 	BlobTxTargetBlobGasPerBlock = 3 * BlobTxBlobGasPerBlob // Target consumable blob gas for data blobs per block (for 1559-like pricing)
-	MaxBlobGasPerBlock          = 6 * BlobTxBlobGasPerBlob // Maximum consumable blob gas for data blobs per block
+	MaxBlobGasPerBlock          = 5 * BlobTxBlobGasPerBlob // Maximum consumable blob gas for data blobs per block
 
 	HistoryServeWindow = 8192 // Number of blocks to serve historical block hashes for, EIP-2935.
 )
